3354ed5712b532c19abbe51d4c959a5cb0530dbf
teki.sei <teki.sei@uzabase.com>
2026-01-30 07:34:30 +0900

    label: score

--- lib/search-evaluation/src/domain/label.rs
+++ lib/search-evaluation/src/domain/label.rs
@@ -1,32 +1,51 @@
 use libm::log2;
-use std::collections::HashMap;
+use std::cmp::Ordering;
 
+#[derive(Debug, PartialEq)]
 pub struct Labels {
     pub list: Vec<Label>,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Label {
     pub query_id: String,
     pub ideal_results: Vec<IdealResult>,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct IdealResult {
     pub document_id: String,
     pub impairment_gain: f64,
 }
 
 impl Label {
-    pub fn score(&self) -> f64 {
-        todo!()
+    pub fn score(&mut self) -> f64 {
+        self.sort_ideal_results();
+        let mut score = 0.0;
+        for (index, ir) in self.ideal_results.iter().enumerate() {
+            score += log2(index as f64 + 2.0) * ir.impairment_gain;
+        }
+        score
+    }
+
+    fn sort_ideal_results(&mut self) {
+        self.ideal_results.sort_by(|left, right| {
+            right
+                .impairment_gain
+                .partial_cmp(&left.impairment_gain)
+                .unwrap_or(Ordering::Equal)
+        })
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use approx::assert_relative_eq;
+
     #[test]
     fn test_label_score() {
-        let label = Label {
+        let mut label = Label {
             query_id: "query1".to_string(),
             ideal_results: vec![
                 IdealResult {
@@ -55,7 +74,7 @@ mod tests {
                 },
             ],
         };
-        relative_eq!(
+        assert_relative_eq!(
             label.score(),
             log2(2.0) * 1.0
                 + log2(3.0) * 1.0
--- lib/search-evaluation/src/lib.rs
+++ lib/search-evaluation/src/lib.rs
@@ -1,11 +1,3 @@
-#[macro_use]
-extern crate approx;
-
 mod domain;
 
 // pub fn evaluate(labels: Labels, predictions: Predictions) -> EvaluationResults {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-}
